867debf4ca4e860470d469d279d8f7edafd29c8c
MANTM1224 <etorresm2304@alumno.ipn.mx>
2025-12-09 13:44:49 -0600

    La base para agregar el servidor

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,14 @@
 //Los quiero mucho amigos este es mi ultimo acto de amor hacia ustedes
+import express from 'express';
+import path from 'path';
+
+// Rutas que se van a consumir 
+
+const app = require('express')();
+const PORT = process.env.PORT || 3000;      
+app.use(express.json());
+
+//Comenzar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
