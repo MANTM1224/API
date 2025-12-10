ebd05a0742bd8f27a3f352d00fc31e8cd99feb9b
ViewedRapier708 <jdelenac2300@alumno.ipn.mx>
2025-12-09 19:07:33 -0600

    DB

---
+++ db.sql
@@ -0,0 +1,70 @@
+CREATE TABLE `api`.`usuario` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `UserName` VARCHAR(255) NOT NULL,
+  `Correo` VARCHAR(255) NULL,
+  `Contraseña` VARCHAR(255) NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE INDEX `ID_UNIQUE` (`ID` ASC) VISIBLE,
+  UNIQUE INDEX `UserName_UNIQUE` (`UserName` ASC) VISIBLE,
+  UNIQUE INDEX `Correo_UNIQUE` (`Correo` ASC) VISIBLE);
+
+  CREATE TABLE `api`.`categoria` (
+  `ID` INT NOT NULL,
+  `Nombre` VARCHAR(255) NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE INDEX `ID_UNIQUE` (`ID` ASC) VISIBLE);
+
+CREATE TABLE `api`.`inventario` (
+  `ID` INT NOT NULL,
+  `Nombre` VARCHAR(255) NULL,
+  `Precio` FLOAT NULL,
+  `Stock` INT NULL,
+  `Categoria` INT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE INDEX `ID_UNIQUE` (`ID` ASC) VISIBLE,
+  INDEX `cat_idx` (`Categoria` ASC) VISIBLE,
+  CONSTRAINT `cat`
+    FOREIGN KEY (`Categoria`)
+    REFERENCES `api`.`categoria` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+    CREATE TABLE `api`.`carrito` (
+  `ID` INT NOT NULL,
+  `usuario` INT NOT NULL,
+  `producto` INT NOT NULL,
+  `cantidad` INT NOT NULL,
+  PRIMARY KEY (`ID`),
+  INDEX `intentario_idx` (`producto` ASC) VISIBLE,
+  INDEX `usuario_idx` (`usuario` ASC) VISIBLE,
+  CONSTRAINT `usuario`
+    FOREIGN KEY (`usuario`)
+    REFERENCES `api`.`usuario` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `intentario`
+    FOREIGN KEY (`producto`)
+    REFERENCES `api`.`inventario` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+CREATE TABLE `api`.`ventas` (
+  `ID` INT NOT NULL,
+  `usuario` INT NULL,
+  `fecha de compra` DATE NOT NULL,
+  `producto` INT NULL,
+  `cantidad` INT NULL,
+  `precio total` FLOAT NULL,
+  PRIMARY KEY (`ID`),
+  INDEX `usuario_idx` (`usuario` ASC) VISIBLE,
+  INDEX `producto_idx` (`producto` ASC) VISIBLE,
+  CONSTRAINT `usuario`
+    FOREIGN KEY (`usuario`)
+    REFERENCES `api`.`usuario` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `producto`
+    FOREIGN KEY (`producto`)
+    REFERENCES `api`.`inventario` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
