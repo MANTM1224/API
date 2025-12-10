b9e87e29dd32bb4b68e2665225a30c5f1805c87c
ViewedRapier708 <jdelenac2300@alumno.ipn.mx>
2025-12-09 18:06:55 -0600

    first commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,27 @@
-//Los quiero mucho amigos este es mi ultimo acto de amor hacia ustedes
 import express from 'express';
 import path from 'path';
+//aqui nosotros tenemos que agregar las rutas que se van a consumir
+import productroutes from './routes/productroutes.js';
 
-// Rutas que se van a consumir 
 
-//Comenzar el servidor
 const app = express();
-const PORT = process.env.PORT || 3000;      
+const PORT = process.env.PORT || 3000;  
+
+const __dirname = path.resolve(); // Obtener el directorio actual
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname,  '../Frontend', 'public')));
+
+app.set('views engine', 'ejs');
+app.set('public', path.join(__dirname, '../Frontend', 'public'));
+
+//vamos a consumir las rutas
+app.use('/',(req, res) => {
+ res.sendFile(path.join(__dirname, '../Frontend', 'public', 'index.html'));
+} 
+    
+);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
