d8c0919363c66ce9d8ee3af02b1056f85e79b57e
ViewedRapier708 <jdelenac2300@alumno.ipn.mx>
2025-12-12 14:47:02 -0600

    inventario

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,22 +1,27 @@
 import { Router } from 'express';
-import {
-	CreateUser,
-	Loginuser,
-	getUsers,
-	getUserById,
-	updateUser,
-	deleteUser
-} from '../controllers/usercontroller.js';
+import { 
+    getInventario,
+    renderCarrito,
+    renderCuenta,
+    renderEditarCuenta,
+    renderCrearAcc,
+    renderIniciarSs,
+    renderManipularProductos,
+    renderHistorial
+} from '../controllers/inventarioController.js';
 
 const router = Router();
 
+// Página principal
+router.get('/', getInventario);
 
-// CRUD usuarios
-router.get('/', getUsers);
-router.get('/:id', getUserById);
-router.post('/register', CreateUser);
-router.post('/login', Loginuser);
-router.put('/:id', updateUser);
-router.delete('/:id', deleteUser);
+// Rutas de vistas
+router.get('/carrito/:usuarioID', renderCarrito);
+router.get('/cuenta/:usuarioID', renderCuenta);
+router.get('/editarCuenta/:usuarioID', renderEditarCuenta);
+router.get('/crearAcc', renderCrearAcc);
+router.get('/iniciarSs', renderIniciarSs);
+router.get('/manipularProductos', renderManipularProductos);
+router.get('/historial/:usuarioID', renderHistorial);
 
 export default router;
